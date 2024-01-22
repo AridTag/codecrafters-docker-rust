b584f0968359993c8a0ed13d73d9b1a0787caf46
AridTag <AridTag@binarydepths.com>
2024-01-22 18:00:41 -0500

    stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::process::Stdio;
 use anyhow::{Context, Result};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -10,21 +11,23 @@ fn main() -> Result<()> {
     let command = &args[3];
     let command_args = &args[4..];
     let output = std::process::Command::new(command)
-         .args(command_args)
-         .output()
-         .with_context(|| {
-             format!(
-                 "Tried to run '{}' with arguments {:?}",
-                 command, command_args
-             )
-         })?;
+        .args(command_args)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .output()
+        .with_context(|| {
+            format!(
+                "Tried to run '{}' with arguments {:?}",
+                command, command_args
+            )
+        })?;
 
-     if output.status.success() {
-         let std_out = std::str::from_utf8(&output.stdout)?;
-         println!("{}", std_out);
-     } else {
-         std::process::exit(1);
-     }
+    if output.status.success() {
+        let std_out = std::str::from_utf8(&output.stdout)?;
+        println!("{}", std_out);
+    } else {
+        std::process::exit(1);
+    }
 
     Ok(())
 }
